d57c812f969d964872ebec2cb5d092daec167153
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-05-21 17:21:56 +0200

    testing

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 const database = mysql.createPool({
     host: 'localhost',
     port: '3306',
@@ -23,7 +22,6 @@ const database = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
 })
-
 
 function query(sql, values) {
     return new Promise((resolve, reject) => {
@@ -36,27 +34,25 @@ function query(sql, values) {
     });
 }
 
-
 app.listen(port, () => {
     console.log(`Server running on port, http://localhost:${port}`);
 })
 
-// Get 
+//
 app.get('/', async (req, res) => {
     const title = "Homepage";
     res.render('index', { title });
 });
 
-
 // Get students
 app.get('/students', async (req, res) => {
+    // const query = `SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'students' AND TABLE_SCHEMA = 'GritAcademy';`;
+
     const title = "Students";
     const students = await query("SELECT * FROM students;");
     console.log(students);
     res.render('partials/showStudents', { title, students });
 });
-
-
 
 // Get courses
 app.get('/courses', async (req, res) => {
@@ -65,19 +61,3 @@ app.get('/courses', async (req, res) => {
     console.log(courses);
     res.render('partials/showCourses', { title, courses });
 });
-
-// A function that generates a table for the data
-// function buildTable(columns) {
-//     // Create all columns
-//     let columnSize = "";
-//     // Create column for every column
-//     for (let i = 0; i < columns.length; i++) {
-//         // If columns length is bigger then i take away 1
-//         if (i < columns.length - 1) {
-//             columnSize += columns[i] + ",";
-//         } else {
-//             columnSize += columns[i];
-//         }
-//     }
-//     return columnSize;
-// }
